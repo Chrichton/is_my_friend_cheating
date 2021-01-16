b105fa33c766c385db754db990954ebb80eec87d
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-01-16 04:29:23 +0100

    optimized

--- lib/is_my_friend_cheating.ex
+++ lib/is_my_friend_cheating.ex
@@ -4,27 +4,21 @@ defmodule IsMyFriendCheating do
   """
 
   def remov_nb(n) do
-    array = Enum.to_list(1..n)
-
-    comb(2, array)
-    |> Enum.filter(fn [a, b] -> excluded_numbers?(array, {a, b}) end)
+    1..n
+    |> Enum.to_list()
+    |> comb(2)
+    |> Enum.filter(fn [a, b] -> excluded_numbers?(n, {a, b}) end)
     |> Enum.flat_map(fn [a, b] -> [{a, b}, {b, a}] end)
   end
 
-  def excluded_numbers?(array, {a, b}) do
-    a * b == sum_except(array, {a, b})
+  def excluded_numbers?(n, {a, b}) do
+    a * b == n / 2 * (n + 1) - a - b
   end
 
-  def sum_except(array, {a, b}) do
-    array
-    |> Enum.filter(&(&1 != a && &1 != b))
-    |> Enum.sum()
-  end
+  def comb(_, 0), do: [[]]
+  def comb([], _), do: []
 
-  def comb(0, _), do: [[]]
-  def comb(_, []), do: []
-
-  def comb(m, [h | t]) do
-    for(l <- comb(m - 1, t), do: [h | l]) ++ comb(m, t)
+  def comb([h | t], m) do
+    for(l <- comb(t, m - 1), do: [h | l]) ++ comb(t, m)
   end
 end
