ac8a6d193ffb2c5fa3ad48baa4f892d260b84799
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-01-17 08:12:28 +0100

    comb now recursive. Starts with ceil(n)
    TODO: prune end

--- lib/is_my_friend_cheating.ex
+++ lib/is_my_friend_cheating.ex
@@ -4,21 +4,24 @@ defmodule IsMyFriendCheating do
   """
 
   def remov_nb(n) do
-    1..n
-    |> Enum.to_list()
-    |> comb(2)
-    |> Enum.filter(fn [a, b] -> excluded_numbers?(n, {a, b}) end)
-    |> Enum.flat_map(fn [a, b] -> [{a, b}, {b, a}] end)
+    comb(n)
+    |> Enum.flat_map(fn {a, b} -> [{a, b}, {b, a}] end)
   end
 
-  def excluded_numbers?(n, {a, b}) do
-    a * b == n / 2 * (n + 1) - a - b
+  def comb(n) do
+    ceil = ceil(n / 2)
+    comb_recursive(ceil, ceil + 1, n / 2 * (n + 1), n, [])
   end
 
-  def comb(_, 0), do: [[]]
-  def comb([], _), do: []
+  def comb_recursive(a, _b, _sum, n, acc) when a >= n,
+    do: Enum.reverse(acc)
 
-  def comb([h | t], m) do
-    for(l <- comb(t, m - 1), do: [h | l]) ++ comb(t, m)
-  end
+  def comb_recursive(a, b, sum, n, acc) when a * b + a + b == sum,
+    do: comb_recursive(a, b + 1, sum, n, [{a, b} | acc])
+
+  def comb_recursive(a, b, sum, n, acc) when b > n,
+    do: comb_recursive(a + 1, a + 2, sum, n, acc)
+
+  def comb_recursive(a, b, sum, n, acc),
+    do: comb_recursive(a, b + 1, sum, n, acc)
 end
